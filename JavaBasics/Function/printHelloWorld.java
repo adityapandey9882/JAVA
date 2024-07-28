258c3c2673521f9b0349e5fe78bff9de12aadd38
Aditya Pandey <vish87717@gmail.com>
2024-07-28 22:21:09 +0530

    java code

---
+++ JavaBasics/Function/printHelloWorld.java
@@ -0,0 +1,9 @@
+package JavaBasics.Function;
+public class printHelloWorld {
+    public static void printHelloWorld() {
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        printHelloWorld();
+    }
+}
