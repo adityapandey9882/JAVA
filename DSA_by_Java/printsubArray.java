7297d96fbe33cc8556bc17e4d84d79743016e13c
Aditya Pandey <vish87717@gmail.com>
2024-08-21 16:19:43 +0530

    java code

---
+++ DSA_by_Java/printsubArray.java
@@ -0,0 +1,15 @@
+package DSA_by_Java;
+
+public class printsubArray {
+    public static void subArray(int numbers){
+        for(int i=0; i<numbers.length; i++){
+            int curr = numbers[i];
+            for(int j=j+1; j<numbers.length; j++)
+        }
+    }
+
+    public static void main(String[] args) {
+        int [] numbers = {2,5,7,8,6,9};
+
+    }
+}
